1b98e412f5b9da0f938cc4054ab70c7f0064ac20
chat263 <130237209+chat263@users.noreply.github.com>
2025-08-21 14:29:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
-!curl -L -o tm https://github.com/chat263/web/releases/download/1/tm
-!curl -L -o cube https://gitlab.hk/chathouse/chathousedocker/-/raw/main/cube?inline=false
-!chmod +x tm cube
 
-!./tm -F
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
